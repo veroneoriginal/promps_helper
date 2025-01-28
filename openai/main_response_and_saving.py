1035d68c0caf677f22d0726e5433afc464240ab3
Veronika <veronikanizovaya@gmail.com>
2025-01-28 08:35:00 +0300

    edits openai logic

--- openai/main_response_and_saving.py
+++ openai/main_response_and_saving.py
@@ -1,78 +1,21 @@
 """
-В этом модуде вызов ключевых функций - от забора данных из таблицы до отправки в openai
+В этом модуде отправка запроса в openai и сохранение итогового файла
 """
 
 import os
 from datetime import datetime
 from dotenv import load_dotenv
 
-from openai.formation_context import formation_context
 from openai.openai_func import generate_text_content_openai
-from prompt_constructor.constructor import PromptConstructor
-from prompt_constructor.settings_constructor.settings_response import SETTINGS_RESPONSE
-from prompt_constructor.settings_constructor.system_prompt import SYSTEM_PROMPT
+
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 
-
-def create_context_for_request_to_openai():
-    """
-    В этой функции собраны все этапы с момента забора информации из таблицы
-    до создания готового контекста, который передается в OpenAI
-    """
-
-    print('Забираю данные для анализа из таблицы.')
-    # скрипт проходит по таблице, и забирает данные из ячеек, формируя словарь ключ-значение,
-    # где ключ это название столбца, а значение - содержимое ячейки
-    # словарь будет иметь вид
-    data_with_info_about_user_and_product = {
-        'Пользователь': {
-            "Пол": "содержимое ячейки",
-            "Возраст": "содержимое ячейки",
-            "Тип волос": "содержимое ячейки",
-            "Тип кожи головы": "содержимое ячейки",
-            "Особенности": "содержимое ячейки",
-            "Проблемы или пожелания": "содержимое ячейки",
-        },
-
-        "Средства": {
-            1: {
-                "Название": "содержимое ячейки",
-                "Состав": "содержимое ячейки",
-            },
-            2: {
-                "Название": "содержимое ячейки",
-                "Состав": "содержимое ячейки",
-            },
-            3: {
-                "Название": "содержимое ячейки",
-                "Состав": "содержимое ячейки",
-            }
-        },
-    }
-
-    print('Собираю промпт.')
-    instance_create_prompt = PromptConstructor()
-    prompt = instance_create_prompt.construct_prompt(
-        data=data_with_info_about_user_and_product,
-        settings_response=SETTINGS_RESPONSE,
-    )
-
-    print('Передаю промпт в контекст.')
-    context = formation_context(
-        prompt=prompt,
-        system_prompt=SYSTEM_PROMPT,
-    )
-
-    return context
-
-
-def response_and_save():
-    print('Начинаю процесс')
-    context = create_context_for_request_to_openai()
-
+def response_and_save(
+        context: list
+):
     print('Передаю контекст в generate_text_content_openai.')
     result = generate_text_content_openai(
         api_key=OPENAI_API_KEY,
@@ -82,7 +25,6 @@ def response_and_save():
 
     # вычленяю нужное
     content = result.choices[0].message.content
-    print(content)
 
     # сохраняю в папку history_prompt
     # Получение текущей даты и времени
@@ -100,7 +42,3 @@ def response_and_save():
         file.write(content)
 
     print(f"Файл успешно сохранен: {file_path}")
-
-
-for _ in range(5):
-    response_and_save()
