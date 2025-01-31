e7ccef736c7f557a02cb76830155bbde9c62c587
Veronika <veronikanizovaya@gmail.com>
2025-01-31 21:59:08 +0300

    edis after linter

--- main.py
+++ main.py
@@ -5,13 +5,11 @@
 import os
 from dotenv import load_dotenv
 
-
 from appeal_to_openai.main import main as appeal_to_openai_main
 from load_data.main import main as load_data_main
 from prompt_constructor.constructor import PromptConstructor
 from prompt_constructor.settings_constructor.settings_response import SETTINGS_RESPONSE
 from prompt_constructor.settings_constructor.system_prompt import SYSTEM_PROMPT
-
 
 
 class ControlManager:
@@ -34,24 +32,24 @@ class ControlManager:
 
     def bring_prompt(
             self,
-            data: dict,
+            dict_with_info: dict,
     ) -> str:
         """
         В этой функции осуществляется вызов функции для создания промпта
 
-        :param data: словарь с описанием параметров пользователя и средствами для анализа
+        :param dict_with_info: словарь с описанием параметров пользователя и средствами для анализа
         :return: промпт в виде строки
         """
 
         instance_create_prompt = PromptConstructor()
         return instance_create_prompt.construct_prompt(
-            data=data,
+            data=dict_with_info,
             settings_response=SETTINGS_RESPONSE,
         )
 
     def create_context_for_request_to_openai(
             self,
-            prompt: str,
+            prompt_for_convert: str,
     ) -> None:
         """
         В этой функции осуществляется вызов ключевой функции по:
@@ -59,7 +57,7 @@ class ControlManager:
         2) отправке самого запроса в OpenAI,
         3) сохранение результата
 
-        :param prompt: промпт для преобразования его в контекст
+        :param prompt_for_convert: промпт для преобразования его в контекст
         :return: None
         """
 
@@ -67,7 +65,7 @@ class ControlManager:
         openai_api_key = os.getenv('OPENAI_API_KEY')
 
         appeal_to_openai_main(
-            prompt=prompt,
+            prompt=prompt_for_convert,
             system_prompt=SYSTEM_PROMPT,
             api_key=openai_api_key,
         )
@@ -80,10 +78,10 @@ if __name__ == '__main__':
     data = instance.take_data_from_the_table(file_path='00_base/00_Средства.xlsx')
 
     print('Собираю промпт.')
-    prompt = instance.bring_prompt(data=data)
+    prompt = instance.bring_prompt(dict_with_info=data)
 
     print('Отправляю запрос в OpenAI.')
-    instance.create_context_for_request_to_openai(prompt=prompt)
+    instance.create_context_for_request_to_openai(prompt_for_convert=prompt)
 
     # print('Разбираю ответ от OpenAI.')
     #
