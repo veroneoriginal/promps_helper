302979bb758d713bd09a45e95080c46bd45eb3f6
Veronika <veronikanizovaya@gmail.com>
2025-04-03 20:31:16 +0300

    go parser

--- start_parser.py
+++ start_parser.py
@@ -2,7 +2,7 @@ from ga_parser.main import start_parser
 
 if __name__ == '__main__':
     start_parser(
-        table_path='00_base/Средства.xlsx',
+        table_path='00_base/Средства_АКТУАЛЬНАЯ.xlsx',
         ws_title='Средства',
         image_dir_path='00_base/products/00_img',
         base_delay=3,
