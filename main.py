d824e3be0f1e40169ef11260de07293b2777867e
Veronika <veronikanizovaya@gmail.com>
2025-04-03 19:33:44 +0300

    add func to analog post

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ if __name__ == '__main__':
         param_dif_products_categories=PARAMETERS_DIF_PRODUCT_CATEGORIES,
     )
     control_manager.create_collection(
-        file_path_tools='00_base/Средства.xlsx',
-        # file_path_tools='00_base/Средства_копия.xlsx',
-        # file_path_collection='00_base/Подборки.xlsx',
-        file_path_collection='00_base/Подборки для тестов.xlsx',
+        file_path_tools='00_base/Средства_АКТУАЛЬНАЯ.xlsx',
+        file_path_collection='00_base/Подборки_мои.xlsx',
         path_to_output_folder='00_base/00_info_for_post/',
         checking_unique=True,
     )
