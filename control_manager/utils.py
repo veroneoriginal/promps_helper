e5f1d9578a7db8d92f2a61d84712e5271e06b25b
Arrivvaa <vv.var.vfx@gmail.com>
2025-04-08 23:04:45 +0300

    refactor

--- control_manager/utils.py
+++ control_manager/utils.py
@@ -7,7 +7,8 @@ def create_dict_from_str(_str: str) -> dict:
     Конвертирует строку в словарь, если позволяет структура.
     Перед этим заменят неправильные кавычки на правильные
     """
-
+    if not isinstance(_str, str):
+        raise ValueError(f"Ожидалась строка, но получено: {type(_str)} -> {_str}")
     # Заменяем нестандартных кавычек на обычные
     normalized_products = re.sub(r'[«»]', '"', _str)
     return ast.literal_eval(normalized_products)
